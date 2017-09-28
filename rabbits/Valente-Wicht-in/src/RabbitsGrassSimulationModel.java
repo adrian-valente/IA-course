b4ab906ec91f934fac94c6baa4fe65c8aed960da
Bruno Wicht <bruno.wicht@epfl.ch>
2017-09-28 15:06:22 +0200

    displaying map working

--- rabbits/Valente-Wicht-in/src/RabbitsGrassSimulationModel.java
+++ rabbits/Valente-Wicht-in/src/RabbitsGrassSimulationModel.java
@@ -1,5 +1,11 @@
+import java.awt.Color;
+
 import uchicago.src.sim.engine.Schedule;
+import uchicago.src.sim.engine.SimInit;
 import uchicago.src.sim.engine.SimModelImpl;
+import uchicago.src.sim.gui.ColorMap;
+import uchicago.src.sim.gui.DisplaySurface;
+import uchicago.src.sim.gui.Value2DDisplay;
 
 /**
  * Class that implements the simulation model for the rabbits grass
@@ -13,11 +19,29 @@ import uchicago.src.sim.engine.SimModelImpl;
 
 public class RabbitsGrassSimulationModel extends SimModelImpl {		
 
+		private static final int XSIZE = 20;
+		private static final int YSIZE = 20;
+		private static final int NUMAGENTS = 10;
+		private static final int BIRTH_THRESHOLD = 3;
+		private static final int GRASS_GROWTH_RATE = 5;
+		
+		private int x = XSIZE;
+		private int y = YSIZE;
+		private int numAgents = NUMAGENTS;
+		private int birthThreshold = BIRTH_THRESHOLD;
+		private int grassGrowth = GRASS_GROWTH_RATE;
+		
 		private Schedule schedule;
-		private int numAgents;
+		
+		private RabbitsGrassSimulationSpace space;
+		
+		private DisplaySurface surface;
 	
 		public static void main(String[] args) {
 			
+			SimInit init = new SimInit();
+			RabbitsGrassSimulationModel model = new RabbitsGrassSimulationModel();
+			init.loadModel(model, "", false);
 			System.out.println("Rabbit skeleton");
 			
 		}
@@ -27,7 +51,15 @@ public class RabbitsGrassSimulationModel extends SimModelImpl {
 		}
 		
 		public void setup() {
-			// TODO Auto-generated method stub
+			space = null;
+			
+			if(surface != null) {
+				surface.dispose();
+			}
+			surface = null;
+			
+			surface = new DisplaySurface(this, "Rabbits love grass");
+			registerDisplaySurface("Rabbits love grass", surface);
 			
 		}
 
@@ -35,15 +67,26 @@ public class RabbitsGrassSimulationModel extends SimModelImpl {
 			buildModel();
 			buildSchedule();
 			buildDisplay();	
+			
+			surface.display();
 		}
 		
 		public void buildModel(){
+			space = new RabbitsGrassSimulationSpace(x,y);
+			space.spreadGrass(grassGrowth);
 		}
 
 		public void buildSchedule(){
 		}
 
 		public void buildDisplay(){
+			ColorMap map = new ColorMap();
+			for(int i = 1; i < 16; ++i) {
+				map.mapColor(i, new Color(0, ((int)(127 + 8*i)), 0));
+			}
+			map.mapColor(0, Color.WHITE);
+			Value2DDisplay displayGrass = new Value2DDisplay(space.getCurrentGrassSpace(), map);
+			surface.addDisplayable(displayGrass, "Grass");
 		}
 		
 		public Schedule getSchedule() {
@@ -59,7 +102,7 @@ public class RabbitsGrassSimulationModel extends SimModelImpl {
 		}
 
 		public String[] getInitParam() {
-			String[] initParam = {"numAgents"};
+			String[] initParam = {"numAgents", "x", "y", "birthThreshold", "grassGrowth"};
 			return initParam;
 		}	
 }
--- rabbits/Valente-Wicht-in/src/RabbitsGrassSimulationSpace.java
+++ rabbits/Valente-Wicht-in/src/RabbitsGrassSimulationSpace.java
@@ -1,8 +1,42 @@
+import uchicago.src.sim.space.Discrete2DSpace;
+import uchicago.src.sim.space.Object2DGrid;
+
 /**
  * Class that implements the simulation space of the rabbits grass simulation.
  * @author 
  */
 
 public class RabbitsGrassSimulationSpace {
+	private Object2DGrid grassSpace;
+	
+	public RabbitsGrassSimulationSpace(int x, int y) {
+		grassSpace = new Object2DGrid(x,y);
+		for(int i = 0; i < x; ++i) {
+			for(int j = 0; j < y; ++j) {
+				grassSpace.putObjectAt(i, j, new Integer(0));
+			}
+		}
+	}
+	
+	public void spreadGrass(int grass) {
+		for(int i = 0; i < grass; ++i) {
+			
+			int x = (int)(Math.random()*(grassSpace.getSizeX()));
+			int y = (int)(Math.random()*(grassSpace.getSizeY()));
+			
+			int sum;
+			if(grassSpace.getObjectAt(x, y)!=null) {
+				sum = ((Integer)(grassSpace.getObjectAt(x, y))).intValue();
+			}
+			else {
+				sum = 0;
+			}
+			
+			grassSpace.putObjectAt(x, y, new Integer(sum+1));
+		}
+	}
 
+	public Discrete2DSpace getCurrentGrassSpace() {
+		return grassSpace;
+	}
 }
