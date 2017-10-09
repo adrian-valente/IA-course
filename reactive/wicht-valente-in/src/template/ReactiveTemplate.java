50f2d0ff0f5ccd80bc4c251eb90627049d92cd5d
Bruno Wicht <bruno.wicht@epfl.ch>
2017-10-09 16:22:04 +0200

    Reactive agent working

--- reactive/wicht-valente-in/src/template/ReactiveTemplate.java
+++ reactive/wicht-valente-in/src/template/ReactiveTemplate.java
@@ -1,55 +1,140 @@
 package template;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
-
+import template.State;
 import logist.simulation.Vehicle;
 import logist.agent.Agent;
 import logist.behavior.ReactiveBehavior;
 import logist.plan.Action;
-import logist.plan.Action.Move;
 import logist.plan.Action.Pickup;
 import logist.task.Task;
 import logist.task.TaskDistribution;
 import logist.topology.Topology;
 import logist.topology.Topology.City;
 
-public class ReactiveTemplate implements ReactiveBehavior {
 
+public class ReactiveTemplate implements ReactiveBehavior{
 	private Random random;
-	private double pPickup;
+	private double discount;
 	private int numActions;
 	private Agent myAgent;
+	private State state;
+	private ArrayList<State> states;
+	private HashMap<State,City> best;
+	private final double cost = 0.1;
 
 	@Override
 	public void setup(Topology topology, TaskDistribution td, Agent agent) {
-
-		// Reads the discount factor from the agents.xml file.
-		// If the property is not present it defaults to 0.95
-		Double discount = agent.readProperty("discount-factor", Double.class,
-				0.95);
-
+		this.discount = (double)agent.readProperty("discount-factor", Double.class,0.95);
 		this.random = new Random();
-		this.pPickup = discount;
 		this.numActions = 0;
 		this.myAgent = agent;
+		this.states = generateStates(topology);
+		this.best = new HashMap<State,City>();
+		
+		learn(topology,td);
+		System.out.println(this.best);
+	}
+	
+	public void learn(Topology topology, TaskDistribution td){
+		System.out.println("Learning...");
+		//HashMap<State,HashMap<Action,Double>> Q = new HashMap<State,HashMap<Action,Double>>();
+		HashMap<State,Double> V = new HashMap<State,Double>();
+		double qmax;
+		City argmaxQ;
+		//Build V 
+		for (State s : states){
+			V.put(s, new Double(0));
+		}
+		
+		boolean change = true;
+		int i = 0;
+		
+		//Learning
+		while(change){
+			change = false;
+			++i;
+			for (State s : states){
+				qmax = Double.MIN_VALUE;
+				argmaxQ = null;
+				//Action pickup
+				if (s.getTaskDest() != null){
+					long q = td.reward(s.getPosition(), s.getTaskDest());
+					q -= s.getPosition().distanceTo(s.getTaskDest()) * cost;
+					for (State s2 : states){
+						if (s2.getPosition() == s.getTaskDest()){
+							q += discount*td.probability(s.getTaskDest(), s2.getTaskDest())*V.get(s2);
+							
+						}
+					}
+					qmax = q;
+				}
+				//Actions Move
+				for (City dest : s.getPosition().neighbors()){
+					double q = 0;
+					q -= s.getPosition().distanceTo(dest) * cost;
+					for (State s2 : states){
+						if (s2.getPosition() == dest){
+							q += discount*td.probability(dest, s2.getTaskDest())*V.get(s2);
+							
+						}
+					}
+					if (q > qmax){
+						qmax = q;
+						argmaxQ = dest;
+					}
+				}
+				best.put(s, argmaxQ);
+				if(V.get(s) != qmax) {
+					change = true;
+				}
+				V.put(s, qmax);
+			}
+		}
+
+		System.out.println(i);
+	}
+	
+	public ArrayList<State> generateStates(Topology topology){
+		ArrayList<State> l = new ArrayList<State>();
+		for (City pos : topology.cities()){
+			l.add(new State(pos,null));
+			for (City dest : topology.cities()){
+				l.add(new State(pos,dest));
+			}
+		}
+		return l;
 	}
 
 	@Override
 	public Action act(Vehicle vehicle, Task availableTask) {
 		Action action;
-
-		if (availableTask == null || random.nextDouble() > pPickup) {
-			City currentCity = vehicle.getCurrentCity();
-			action = new Move(currentCity.randomNeighbor(random));
-		} else {
-			action = new Pickup(availableTask);
+		if (availableTask == null)
+			state = findState(vehicle.getCurrentCity(), null);
+		else {
+			state = findState(vehicle.getCurrentCity(), availableTask.deliveryCity);
 		}
+		action = new Action.Move(state.getPosition().randomNeighbor(random));
+		if(best.containsKey(state)) {
+			if(best.get(state) == null) {
+				action = new Pickup(availableTask);
+			}	
+		}
+		System.out.println("The total profit after "+numActions+" actions is "+myAgent.getTotalProfit()+" (average profit: "+(myAgent.getTotalProfit() / (double)numActions)+")");
 		
-		if (numActions >= 1) {
-			System.out.println("The total profit after "+numActions+" actions is "+myAgent.getTotalProfit()+" (average profit: "+(myAgent.getTotalProfit() / (double)numActions)+")");
-		}
 		numActions++;
 		
 		return action;
+	}
+	
+	private State findState(City src, City dest) {
+		for(State s: states) {
+			if(s.equals(new State(src,dest))) {
+				return s;
+			}
+		}
+		return null;
 	}
 }
