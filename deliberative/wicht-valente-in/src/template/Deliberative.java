62ef94f9479783ce0ee0a31bf034d032b6049332
Bruno Wicht <bruno.wicht@epfl.ch>
2017-10-19 16:51:47 +0200

    transition fonction

--- deliberative/wicht-valente-in/src/template/Deliberative.java
+++ deliberative/wicht-valente-in/src/template/Deliberative.java
@@ -35,12 +35,35 @@ public class Deliberative implements DeliberativeBehavior {
 	public ArrayList<State> transit(State s, Vehicle v) {
 		ArrayList<State> states = new ArrayList<State>();
 		City c = s.getCity();
-		int freeCharge = 0;
-		for(City n: c.neighbors()) {
-			states.add(s.move(n));
+		int freeCharge = v.capacity()-s.weight();
+		
+		//if there is no task carried, try to move to each neighbor city
+		if(s.getToDeliver().isEmpty()) {
+			for(City n: c.neighbors()) {
+				states.add(s.move(n));
+			}
 		}
+		
+		//try to move to the next city on the path to the delivery city of the first task no taking any new task
+		else {
+			Task t = s.getToDeliver().get(0);
+			City next = c.pathTo(t.deliveryCity).get(0);
+			states.add(s.move(next));
+		}
+		
+		//try to pickup each task available in the current city
 		for(Task t: s.localPickupTasks()) {
-			if()
+			if(freeCharge >= t.weight) {
+				states.add(s.pickup(t));
+			}
+		}
+		
+		//for each successor state, deliver tasks if possible
+		for(State state: states) {
+			ArrayList<Task> local = state.localDeliverTask();
+			for(Task t: local) {
+				state = state.deliver(t);
+			}
 		}
 		
 		return states;
